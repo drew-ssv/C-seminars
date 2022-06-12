d4c9c679d24ca3353cbeea78ec044098e9b5db90
drew-ssv <drew.semenov@gmail.com>
2022-06-12 22:17:37 +0300

    Proverka_kvadrata_chisla

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// проверка квадрата числа
+Console.Write("Введите число №1: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число №2: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+
+if (Number1 * Number1 == Number2)
+{
+  Console.WriteLine(" правильно, квадрат числа " + Number1 + " = " + Number2);   
+}
+else
+{
+    Console.WriteLine(" число " + Number2 + " не является квадратом числа " +  Number1);   
+}
+   
